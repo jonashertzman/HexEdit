918a017a968192c05b35ce01d092b27e724801fa
Jonas Hertzman <jonas.hertzman@gmail.com>
2019-12-27 19:13:31 +0100

    View model

---
+++ HexEdit/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+
+namespace HexEdit
+{
+	public class MainWindowViewModel : INotifyPropertyChanged
+	{
+
+		#region Properties
+
+		public string Title
+		{
+			get { return "Hex Edit"; }
+		}
+
+		public string Version
+		{
+			get { return "Alpha 1"; }
+		}
+
+		public string BuildNumber
+		{
+			get
+			{
+				DateTime buildDate = new FileInfo(Assembly.GetExecutingAssembly().Location).LastWriteTime;
+				return $"{buildDate.ToString("yy")}{buildDate.DayOfYear}";
+			}
+		}
+
+		public string FullApplicationName
+		{
+			get { return $"{Title} {Version}  (Build {BuildNumber})"; }
+		}
+
+		#endregion
+
+		#region INotifyPropertyChanged
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChanged(string name)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+		}
+
+		#endregion
+
+	}
+}
