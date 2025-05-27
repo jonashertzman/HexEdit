860545a0135a6905ee90c0be09dfa519c1ef01fe
Jonas Hertzman <jonas.hertzman@gmail.com>
2025-05-27 16:37:08 +0200

    No invalid charaters

--- HexEdit/Chunk.cs
+++ HexEdit/Chunk.cs
@@ -5,201 +5,204 @@ namespace HexEdit;
 public class Chunk : INotifyPropertyChanged
 {
 
-	#region Constructor
+    #region Constructor
 
-	public Chunk(ChunkType type, int start, byte[] bytes)
-	{
-		Type = type;
-		Start = start;
-		Length = bytes.Length;
+    public Chunk(ChunkType type, int start, byte[] bytes)
+    {
+        Type = type;
+        Start = start;
+        Length = bytes.Length;
 
-		switch (type)
-		{
-			case ChunkType.Utf8Character:
-				UnicodeCharacter = DecodeUtf8(bytes);
-				break;
+        switch (type)
+        {
+            case ChunkType.Utf8Character:
+                UnicodeCharacter = DecodeUtf8(bytes);
+                break;
 
-			case ChunkType.Utf16leCharacter:
-				UnicodeCharacter = DecodeUtf16le(bytes);
-				break;
+            case ChunkType.Utf16leCharacter:
+                UnicodeCharacter = DecodeUtf16le(bytes);
+                break;
 
-			case ChunkType.Utf16beCharacter:
-				UnicodeCharacter = DecodeUtf16be(bytes);
-				break;
+            case ChunkType.Utf16beCharacter:
+                UnicodeCharacter = DecodeUtf16be(bytes);
+                break;
 
-			case ChunkType.Utf32leCharacter:
-				UnicodeCharacter = DecodeUtf32le(bytes);
-				break;
+            case ChunkType.Utf32leCharacter:
+                UnicodeCharacter = DecodeUtf32le(bytes);
+                break;
 
-			case ChunkType.Utf32beCharacter:
-				UnicodeCharacter = DecodeUtf32be(bytes);
-				break;
+            case ChunkType.Utf32beCharacter:
+                UnicodeCharacter = DecodeUtf32be(bytes);
+                break;
 
-		}
-	}
+        }
+    }
 
-	#endregion
+    #endregion
 
-	#region Overrides
+    #region Overrides
 
-	public override string ToString()
-	{
-		return $"{Start} - {Length}   {Type}";
-	}
+    public override string ToString()
+    {
+        return $"{Start} - {Length}   {Type}";
+    }
 
-	#endregion
+    #endregion
 
-	#region Properties
+    #region Properties
 
-	public ChunkType Type { get; internal set; } = ChunkType.None;
+    public ChunkType Type { get; internal set; } = ChunkType.None;
 
-	public int Start { get; internal set; }
+    public int Start { get; internal set; }
 
-	public int UnicodeCharacter { get; internal set; } = -1;
+    public int UnicodeCharacter
+    {
+        get
+        {
+            return field;
+        }
+        internal set
+        {
+            if (value >= 0x0000_0000 && value <= 0x0010_FFFF)
+            {
+                field = value;
+            }
+        }
+    } = -1;
 
-	public int Length { get; private set; }
+    public int Length { get; private set; }
 
-	public int End
-	{
-		get
-		{
-			return Start + Length - 1;
-		}
-	}
+    public int End
+    {
+        get
+        {
+            return Start + Length - 1;
+        }
+    }
 
-	public string PreviewString
-	{
-		get
-		{
-			switch (Type)
-			{
-				case ChunkType.Bom:
-					return "[BOM]";
+    public string PreviewString
+    {
+        get
+        {
+            switch (Type)
+            {
+                case ChunkType.Bom:
+                    return "[BOM]";
 
-				case ChunkType.Utf8Character:
-				case ChunkType.Utf16leCharacter:
-				case ChunkType.Utf16beCharacter:
-				case ChunkType.Utf32leCharacter:
-				case ChunkType.Utf32beCharacter:
-					if (UnicodeCharacter != -1)
-						return char.ConvertFromUtf32(UnicodeCharacter);
-					return "[UNKNOWN]";
+                case ChunkType.Utf8Character:
+                case ChunkType.Utf16leCharacter:
+                case ChunkType.Utf16beCharacter:
+                case ChunkType.Utf32leCharacter:
+                case ChunkType.Utf32beCharacter:
+                    if (UnicodeCharacter != -1)
+                        return char.ConvertFromUtf32(UnicodeCharacter);
+                    return "[UNKNOWN]";
 
 
-				default:
-					return "[UNKNOWN]";
+                default:
+                    return "[UNKNOWN]";
 
-			}
-		}
-	}
+            }
+        }
+    }
 
-	#endregion
+    #endregion
 
-	#region Methods
+    #region Methods
 
-	private int DecodeUtf8(byte[] bytes)
-	{
-		int character = 0;
-		if (bytes.Length == 1)
-		{
-			character = bytes[0] & 0b0111_1111;
-		}
-		else if (bytes.Length == 2)
-		{
-			character = bytes[0] & 0b0001_1111;
-		}
-		else if (bytes.Length == 3)
-		{
-			character = bytes[0] & 0b0000_1111;
-		}
-		else if (bytes.Length == 4)
-		{
-			character = bytes[0] & 0b0000_0111;
-		}
+    private int DecodeUtf8(byte[] bytes)
+    {
+        int character = 0;
+        if (bytes.Length == 1)
+        {
+            character = bytes[0] & 0b0111_1111;
+        }
+        else if (bytes.Length == 2)
+        {
+            character = bytes[0] & 0b0001_1111;
+        }
+        else if (bytes.Length == 3)
+        {
+            character = bytes[0] & 0b0000_1111;
+        }
+        else if (bytes.Length == 4)
+        {
+            character = bytes[0] & 0b0000_0111;
+        }
 
-		int i = 0;
-		while (++i < bytes.Length)
-		{
-			character <<= 6;
-			character |= bytes[i] & 0b0011_1111;
-		}
+        int i = 0;
+        while (++i < bytes.Length)
+        {
+            character <<= 6;
+            character |= bytes[i] & 0b0011_1111;
+        }
 
-		return character;
-	}
+        return character;
+    }
 
-	private int DecodeUtf16le(byte[] bytes)
-	{
-		if (bytes.Length == 2)
-		{
-			return bytes[1] << 8 | bytes[0];
-		}
-		else // 4 byte surrogate pair character
-		{
-			int highSurrogate = bytes[1] << 8 | bytes[0];
-			int lowSurrogate = bytes[3] << 8 | bytes[2];
+    private int DecodeUtf16le(byte[] bytes)
+    {
+        if (bytes.Length == 2)
+        {
+            return bytes[1] << 8 | bytes[0];
+        }
+        else // 4 byte surrogate pair character
+        {
+            int highSurrogate = bytes[1] << 8 | bytes[0];
+            int lowSurrogate = bytes[3] << 8 | bytes[2];
 
-			highSurrogate -= 0xD800;
-			highSurrogate *= 0x400;
-			lowSurrogate -= 0xDC00;
+            highSurrogate -= 0xD800;
+            highSurrogate *= 0x400;
+            lowSurrogate -= 0xDC00;
 
-			return highSurrogate + lowSurrogate + 0x10000;
-		}
-	}
+            return highSurrogate + lowSurrogate + 0x10000;
+        }
+    }
 
-	private int DecodeUtf16be(byte[] bytes)
-	{
-		if (bytes.Length == 2)
-		{
-			return bytes[0] << 8 | bytes[1];
-		}
-		else // 4 byte surrogate pair character
-		{
-			int highSurrogate = bytes[0] << 8 | bytes[1];
-			int lowSurrogate = bytes[2] << 8 | bytes[3];
+    private int DecodeUtf16be(byte[] bytes)
+    {
+        if (bytes.Length == 2)
+        {
+            return bytes[0] << 8 | bytes[1];
+        }
+        else // 4 byte surrogate pair character
+        {
+            int highSurrogate = bytes[0] << 8 | bytes[1];
+            int lowSurrogate = bytes[2] << 8 | bytes[3];
 
-			highSurrogate -= 0xD800;
-			highSurrogate *= 0x400;
-			lowSurrogate -= 0xDC00;
+            highSurrogate -= 0xD800;
+            highSurrogate *= 0x400;
+            lowSurrogate -= 0xDC00;
 
-			return highSurrogate + lowSurrogate + 0x10000;
-		}
-	}
+            return highSurrogate + lowSurrogate + 0x10000;
+        }
+    }
 
-	private int DecodeUtf32le(byte[] bytes)
-	{
-		int i = BitConverter.ToInt32(bytes, 0);
+    private int DecodeUtf32le(byte[] bytes)
+    {
+        int i = BitConverter.ToInt32(bytes, 0);
 
-		if (i >= 0x0000_0000 && i <= 0x0010_FFFF)
-		{
-			return i;
-		}
+        return i;
+    }
 
-		return -1; // Invalid character
-	}
+    private int DecodeUtf32be(byte[] bytes)
+    {
+        int i = bytes[0] << 24 | bytes[1] << 16 | bytes[2] << 8 | bytes[3];
 
-	private int DecodeUtf32be(byte[] bytes)
-	{
-		int i = bytes[0] << 24 | bytes[1] << 16 | bytes[2] << 8 | bytes[3];
+        return i;
+    }
 
-		if (i >= 0x0000_0000 && i <= 0x0010_FFFF)
-		{
-			return i;
-		}
+    #endregion
 
-		return -1; // Invalid character
-	}
+    #region INotifyPropertyChanged
 
-	#endregion
+    public event PropertyChangedEventHandler PropertyChanged;
 
-	#region INotifyPropertyChanged
+    public void OnPropertyChanged(string name)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
 
-	public event PropertyChangedEventHandler PropertyChanged;
-
-	public void OnPropertyChanged(string name)
-	{
-		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-	}
-
-	#endregion
+    #endregion
 
 }
