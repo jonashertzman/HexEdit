16c522a9a805024b371273979af9ee7e050a882c
Jonas Hertzman <jonas.hertzman@gmail.com>
2025-10-17 09:15:25 +0200

    Update publish profile

--- UpdateVersion/Program.cs
+++ UpdateVersion/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace UpdateVersion;
+﻿using System.IO.Compression;
 
-internal class Program
+namespace UpdateVersion;
+
+class Program
 {
-	static void Main(string[] args)
+	static void Main()
 	{
-		Console.WriteLine("Hello, World!");
+		DateTime buildDate = DateTime.Now;
+		string buildNumber = $"{buildDate:yy}{buildDate.DayOfYear:D3}";
+
+		Console.WriteLine($"Updating version to {buildNumber}");
+
+		File.WriteAllText(@"..\docs\download\version.txt", buildNumber);
+
+
+		Console.WriteLine($"Updating download");
+
+		File.Delete(@"..\docs\download\HexEdit.zip");
+
+		using ZipArchive download = ZipFile.Open(@"..\docs\download\HexEdit.zip", ZipArchiveMode.Create);
+		download.CreateEntryFromFile(@".\bin\Publish\HexEdit.exe", "HexEdit.exe");
+		download.CreateEntryFromFile(@"..\LICENSE", "LICENSE");
 	}
 }
